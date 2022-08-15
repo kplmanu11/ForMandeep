5d636ba59dd94c2872f2827d594a36d897150f4e
Shambhu Katwal <kpl.neupane11@gmail.com>
2022-08-15 09:02:13 +0545

    intial commit

---
+++ src/Test.java
@@ -0,0 +1,23 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		String[] car = { "ram", "sita" };
+		
+		String car1 = "ram";
+		
+		car[0] = "Hari";
+		
+	
+		System.out.println(car[0]);
+
+		for (int i = 0; i < car.length; i++) {
+			System.out.println(car[i]);
+		}
+		
+		
+		
+	}
+}
+
---
+++ src/oopsss/Sum.java
@@ -0,0 +1,17 @@
+package oopsss;
+
+public class Sum {
+
+	public float sum(int x, int y) {
+		int z = x + y;
+		return z;
+	}
+
+	public static void main(String[] args) {
+
+		Sum obj = new Sum();
+		//obj.sum(12,34);
+		System.out.println(obj.sum(12,34));
+	}
+
+}
